65664d9a9bd4ed121a64f4f7bd923daa898a372f
Daniel Zemlik <daniel.zemlik@iglu.com>
2019-12-19 13:31:20 +0100

    Added manager contract.

---
+++ src/Caching/Abstract/ICacheManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Caching.Abstract
+{
+    public interface ICacheService
+    {
+        void AddItem<T>(string key, T data);
+        T Get<T>(string key);
+        void RemoveKey(string key);
+    }
+}
