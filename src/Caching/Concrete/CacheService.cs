b5b03fd21bd3177aca4f6975a9d54cfd9973ab48
Daniel Zemlik <daniel.zemlik@iglu.com>
2019-12-19 13:22:41 +0100

    Added cache service.

---
+++ src/Caching/Concrete/CacheService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Caching;
+using Caching.Abstract;
+
+namespace Caching.Concrete
+{
+    public class CacheService : ICacheService
+    {
+        private readonly ICacheconfig _config;
+
+        public CacheManager(ICacheconfig config)
+        {
+            _config = config;
+        }
+        public void AddItem<T>(string key, T data)
+        {
+            var cache = MemoryCache.Default;
+            var results = (T)cache.Get(key);
+            if (results != null) return;
+
+            var cacheItemPolicy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddHours(_config.DefaultCachePeriod)
+            };
+            cache.Add(key, data, cacheItemPolicy);
+        }
+
+        public T Get<T>(string key)
+        {
+            var cache = MemoryCache.Default;
+            var results = (T)cache.Get(key);
+            return results;
+        }
+
+        public void RemoveKey(string key)
+        {
+            var cache = MemoryCache.Default;
+            cache.Remove(key);
+        }
+    }
+}
